bb24dbee063ab225a1852fab66a249eb914f318b
Peter Hajdu <peter@hajdu.im>
2019-10-12 09:38:30 +0200

    add option parser draft

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,33 @@ import Destination
 import Source
 import ReceiveId
 
+import Data.Text
 import qualified Network.AMQP as AMQP(defaultConnectionOpts)
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Opts = MkOpts
+  { exchange :: Text
+  , routingKey :: Text
+  , sourceQueue :: Text
+  }
+
+optionParser :: Parser Opts
+optionParser = MkOpts
+      <$> strOption
+          ( long "exchange-name"
+         <> metavar "TARGET"
+         <> short 'e'
+         <> help "destination exchange" )
+      <*> strOption
+          ( long "routing-key"
+         <> short 'r'
+         <> help "routing key to send messages with" )
+      <*> strOption
+          ( long "queue"
+         <> short 'q'
+         <> help "source queue")
 
 printError :: IO ()
 printError = putStrLn "Unable to initialize rabbitmq environment."
@@ -18,6 +43,7 @@ createEnv pub (rec, ack) = MkEnv pub rec ack
 
 main :: IO ()
 main = do
-  maybePublisher <- createRabbitMqDestination AMQP.defaultConnectionOpts "exchange" "routingKey"
-  maybeSource <- createRabbitMqSource AMQP.defaultConnectionOpts "queue"
+  opts <- execParser (info optionParser (fullDesc <> progDesc "rabbitmq swiss army knife"))
+  maybePublisher <- createRabbitMqDestination AMQP.defaultConnectionOpts (exchange opts) (routingKey opts)
+  maybeSource <- createRabbitMqSource AMQP.defaultConnectionOpts (sourceQueue opts)
   maybe printError runMover (createEnv <$> maybePublisher <*> maybeSource)
